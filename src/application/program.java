a99505f5f52edc9b39b81aaccca4f775336f5913
Lucas Daniel Telesforo <lucas.telesforo@gmail.com>
2020-07-14 21:54:03 -0300

    acresentada msg de boa noite e eliminacap de espacos em branco!

--- src/application/program.java
+++ src/application/program.java
@@ -5,8 +5,7 @@ public class program {
 	public static void main(String[] args) {
 		System.out.println("Bom dia!");
 		System.out.println("Boa tarde !");
-
-
+		System.out.println("Boa noite!");
 	}
 
 }
